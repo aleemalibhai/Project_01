507fed52868bbe1cbc177d7b9bd4e92541def239
aleemalibhai <aleem.alibhai@gmail.com>
2018-02-22 16:37:06 -0500

    worked on SpamFilter and TestFile again

--- src/sample/SpamFilter.java
+++ src/sample/SpamFilter.java
@@ -2,14 +2,17 @@ package sample;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
 
 
 public class SpamFilter{
+    private double hamCount = 0;
+    private double spamCount = 0;
     private File trainDir;
     private File testDir;
-    private TreeMap<String,Double> probabolitySpamGivenWord;
+    private TreeMap<String,Double> prSW;
     private TreeMap<String,Integer> hamFreq;
     private TreeMap<String,Integer> spamFreq;
 
@@ -19,6 +22,7 @@ public class SpamFilter{
         testDir = testDirectory;
         hamFreq = new TreeMap<>();
         spamFreq = new TreeMap<>();
+        prSW = new TreeMap<>();
     }
 
     public void train() throws IOException{
@@ -46,6 +50,7 @@ public class SpamFilter{
                 }
                 // adjusts the ham frequency map according to words found in file
                 if (current.getName().equals("ham")) {
+                    this.hamCount ++;
                     for (String element : words) {
                         if (hamFreq.containsKey(element)) {
                             int oldCount = hamFreq.get(element);
@@ -56,6 +61,7 @@ public class SpamFilter{
                     }
                     // adjusts the spam frequency map according to words found in file
                 } else if (current.getName().equals("spam")) {
+                    this.spamCount ++;
                     for (String element : words) {
                         if (spamFreq.containsKey(element)) {
                             int oldCount = spamFreq.get(element);
@@ -68,8 +74,78 @@ public class SpamFilter{
                 }
             }
         }
-        System.out.print(hamFreq + "\n");
-        System.out.print(spamFreq);
+        // add probabilities from ham file
+        for (Map.Entry<String, Integer> entry: hamFreq.entrySet()){
+            if (!prSW.containsKey(entry.getKey())){
+                if (!spamFreq.containsKey(entry.getKey())){
+                    prSW.put(entry.getKey(), (0 / ((hamFreq.get(entry.getKey())/hamCount)+(0/spamCount))));
+                }else{
+                    prSW.put(entry.getKey(), (spamFreq.get(entry.getKey())/spamCount) / ((hamFreq.get(entry.getKey())/hamCount)+(spamFreq.get(entry.getKey())/spamCount)));
+                }
+            }
+        }
+        // check spamFreq to find probabilities of words not in ham
+        for (Map.Entry<String, Integer> entry: spamFreq.entrySet()){
+            if (!prSW.containsKey(entry.getKey())){
+                if (!hamFreq.containsKey(entry.getKey())){
+                    prSW.put(entry.getKey(), (spamFreq.get(entry.getKey())/spamCount) / ((0/hamCount)+(spamFreq.get(entry.getKey())/spamCount)));
+                }else{
+                    prSW.put(entry.getKey(), (spamFreq.get(entry.getKey())/spamCount) / ((hamFreq.get(entry.getKey())/hamCount)+(spamFreq.get(entry.getKey())/spamCount)));
+                }
+            }
+        }
+    }
+
+    // testing
+    public ArrayList<TestFile> test() throws IOException{
+        // return arraylist
+        ArrayList<TestFile> testedFiles = new ArrayList();
+        // parse through test directory
+        // file array consisting of ham, spam folders
+        File[] contents = testDir.listFiles();
+        for (File current: contents){
+            // file array consisting of all the files in each of the previous folders
+            File[] files = current.listFiles();
+            // parse through files
+            for (File file: files){
+                // instantiate TestFile object
+                TestFile testing = new TestFile();
+                // set the name
+                testing.setFileName(file.getName());
+                // set actuall class
+                if (current.getName().equals("ham")){
+                    testing.setActualClass("ham");
+                }else if (current.getName().equals("spam")){
+                    testing.setActualClass("spam");
+                }
+                // get probability
+                // sum variable
+                double sum = 0;
+                // parse over file and create list of words
+                ArrayList<String> words = new ArrayList();
+                Scanner scanner = new Scanner(file);
+                scanner.useDelimiter("\\s");//"[\s\.;:\?\!,]");//" \t\n.;,!?-/\\");
+                while (scanner.hasNext()) {
+                    String word = scanner.next();
+                    if (isWord(word)) {
+                        // populate word list per file
+                        if (!words.contains(word)) {
+                            words.add(word);
+                        }
+                    }
+                }
+                for (String word: words){
+                    // check if it is in training map, ignore if not present
+                    if (prSW.containsKey(word)){
+                        sum += ((Math.log(1- prSW.get(word)))-(Math.log(prSW.get(word))));
+                    }
+                }
+                testing.setSpamProbability(1/(1+Math.pow(Math.E, sum)));
+
+                testedFiles.add(testing);
+            }
+        }
+        return testedFiles;
     }
 
     private boolean isWord(String word) {
--- src/sample/TestFile.java
+++ src/sample/TestFile.java
@@ -2,19 +2,18 @@ package sample;
 
 import java.text.DecimalFormat;
 public class TestFile {
-    private String filename;
+    private String fileName;
     private double spamProbability;
     private String actualClass;
 
-    public TestFile(String filename,
-    double spamProbability,
-    String actualClass) {
-        this.filename = filename;
+    public TestFile(){}
+    public TestFile(String filename, double spamProbability, String actualClass) {
+        this.fileName = filename;
         this.spamProbability = spamProbability;
         this.actualClass = actualClass;
     }
 
-    public String getFilename() { return this.filename; }
+    public String getFileName() { return this.fileName; }
     public double getSpamProbability() { return this.spamProbability; }
     public String getSpamProbRounded() {
         DecimalFormat df = new DecimalFormat("0.00000");
@@ -22,7 +21,7 @@ public class TestFile {
     }
     public String getActualClass() { return this.actualClass; }
 
-    public void setFilename(String value) { this.filename = value; }
+    public void setFileName(String value) { this.fileName = value; }
     public void setSpamProbability(double val) { this.spamProbability = val; }
     public void setActualClass(String value) { this.actualClass = value; }
 }
